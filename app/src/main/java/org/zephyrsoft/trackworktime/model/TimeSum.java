44ff3893a32432f49a8c9bdc6a1a491a83adb600
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2018-06-21 21:46:18 +0200

    more test assertions for TimeSum

--- app/src/main/java/org/zephyrsoft/trackworktime/model/TimeSum.java
+++ app/src/main/java/org/zephyrsoft/trackworktime/model/TimeSum.java
@@ -105,13 +105,20 @@ public class TimeSum {
 		TimeSum underTest = new TimeSum();
 		underTest.add(0, 75);
 		assertEquals(underTest.toString(), "1:15");
+		assertEquals(underTest.getAsMinutes(), 75);
+
 		underTest.add(2, 65);
 		assertEquals(underTest.toString(), "4:20");
+		assertEquals(underTest.getAsMinutes(), 260);
+
 		underTest.substract(0, 140);
 		assertEquals(underTest.toString(), "2:00");
 		assertEquals(underTest.getAsMinutes(), 120);
+
 		underTest.substract(1, 75);
 		assertEquals(underTest.toString(), "-0:15");
+		assertEquals(underTest.getAsMinutes(), -15);
+
 		underTest.substract(1, 50);
 		assertEquals(underTest.toString(), "-2:05");
 		assertEquals(underTest.getAsMinutes(), -125);
@@ -119,14 +126,20 @@ public class TimeSum {
 		TimeSum positive = new TimeSum();
 		positive.add(2, 30);
 		assertEquals(positive.toString(), "2:30");
+		assertEquals(positive.getAsMinutes(), 150);
+
 		underTest.addOrSubstract(positive);
 		assertEquals(underTest.toString(), "0:25");
+		assertEquals(underTest.getAsMinutes(), 25);
 
 		TimeSum negative = new TimeSum();
 		negative.substract(0, 85);
 		assertEquals(negative.toString(), "-1:25");
+		assertEquals(negative.getAsMinutes(), -85);
+
 		underTest.addOrSubstract(negative);
 		assertEquals(underTest.toString(), "-1:00");
+		assertEquals(underTest.getAsMinutes(), -60);
 
 	}
 
