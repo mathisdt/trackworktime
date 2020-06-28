fde6d6413138ce046f9114e2aff39cdbf4924f59
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-06-28 02:24:38 +0200

    Changed hourMinute() test to use assert library

--- app/src/test/java/org/zephyrsoft/trackworktime/options/DataTypeTest.java
+++ app/src/test/java/org/zephyrsoft/trackworktime/options/DataTypeTest.java
@@ -2,19 +2,17 @@ package org.zephyrsoft.trackworktime.options;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
 import static org.zephyrsoft.trackworktime.options.DataType.HOUR_MINUTE;
 
 public class DataTypeTest {
 
 	@Test
 	public void hourMinute() {
-		boolean valid = HOUR_MINUTE.validate("-1:55");
-		valid &= HOUR_MINUTE.validate("1:00");
-		valid &= HOUR_MINUTE.validate("1:29");
-		valid &= HOUR_MINUTE.validate("37:30");
-		if (!valid) {
-			throw new AssertionError("HOUR_MINUTE");
-		}
+		assertTrue(HOUR_MINUTE.validate("-1:55"));
+		assertTrue(HOUR_MINUTE.validate("1:00"));
+		assertTrue(HOUR_MINUTE.validate("1:29"));
+		assertTrue(HOUR_MINUTE.validate("37:30"));
 	}
 
 }
