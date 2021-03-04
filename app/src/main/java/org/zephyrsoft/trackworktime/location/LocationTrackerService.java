8995d073173fba24dde4ebf29b01f7256b2af284
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2021-03-04 23:19:57 +0100

    remove notification about missing permissions when the tracking method was successfully enabled

--- app/src/main/java/org/zephyrsoft/trackworktime/location/LocationTrackerService.java
+++ app/src/main/java/org/zephyrsoft/trackworktime/location/LocationTrackerService.java
@@ -1,16 +1,16 @@
 /*
  * This file is part of TrackWorkTime (TWT).
- * 
+ *
  * TWT is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * TWT is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with TWT. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -32,91 +32,96 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * The background service providing the location-based tracking without having the activity open.
- * 
+ *
  * @author Mathis Dirksen-Thedens
  */
 public class LocationTrackerService extends Service {
 
-	private static LocationTracker locationTracker = null;
-	private int startId;
+    private static LocationTracker locationTracker = null;
+    private int startId;
 
-	private static final AtomicBoolean isRunning = new AtomicBoolean(false);
+    private static final AtomicBoolean isRunning = new AtomicBoolean(false);
 
-	private Basics basics = null;
+    private Basics basics = null;
 
-	@Override
-	public void onCreate() {
-		Logger.info("creating LocationTrackerService");
-		basics = Basics.getOrCreateInstance(getApplicationContext());
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-			startForeground(Constants.PERSISTENT_TRACKING_ID, basics.createNotificationTracking());
-		}
-		locationTracker = new LocationTracker((LocationManager) getSystemService(Context.LOCATION_SERVICE), basics
-			.getTimerManager(), basics.getExternalNotificationManager(), (AudioManager) getSystemService(Context.AUDIO_SERVICE));
-		// restart if service crashed previously
-		basics.safeCheckLocationBasedTracking();
-	}
+    @Override
+    public void onCreate() {
+        Logger.info("creating LocationTrackerService");
+        basics = Basics.getOrCreateInstance(getApplicationContext());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForeground(Constants.PERSISTENT_TRACKING_ID, basics.createNotificationTracking());
+        }
+        locationTracker = new LocationTracker((LocationManager) getSystemService(Context.LOCATION_SERVICE), basics
+            .getTimerManager(), basics.getExternalNotificationManager(), (AudioManager) getSystemService(Context.AUDIO_SERVICE));
+        // restart if service crashed previously
+        basics.safeCheckLocationBasedTracking();
+    }
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		// do nothing here as we don't bind the service to an activity
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        // do nothing here as we don't bind the service to an activity
+        return null;
+    }
 
-	// START_STICKY : Using this return value, if the OS kills our Service it will recreate it but the Intent that was
-	// sent to the Service isn’t redelivered. In this way the Service is always running
-	// START_NOT_STICKY: If the SO kills the Service it won’t recreate it until the client calls explicitly onStart
-	// command
-	// START_REDELIVER_INTENT: It is similar to the START_STICKY and in this case the Intent will be redelivered to the
-	// service.
-	@Override
-	public int onStartCommand(Intent intent, int flags, @SuppressWarnings("hiding") int startId) {
-		if (intent == null || intent.getExtras() == null) {
-			// something went wrong, quit here
-			return Service.START_NOT_STICKY;
-		}
+    // START_STICKY : Using this return value, if the OS kills our Service it will recreate it but the Intent that was
+    // sent to the Service isn’t redelivered. In this way the Service is always running
+    // START_NOT_STICKY: If the SO kills the Service it won’t recreate it until the client calls explicitly onStart
+    // command
+    // START_REDELIVER_INTENT: It is similar to the START_STICKY and in this case the Intent will be redelivered to the
+    // service.
+    @Override
+    public int onStartCommand(Intent intent, int flags, @SuppressWarnings("hiding") int startId) {
+        if (intent == null || intent.getExtras() == null) {
+            // something went wrong, quit here
+            return Service.START_NOT_STICKY;
+        }
 
-		Double latitude = (Double) intent.getExtras().get(Constants.INTENT_EXTRA_LATITUDE);
-		Double longitude = (Double) intent.getExtras().get(Constants.INTENT_EXTRA_LONGITUDE);
-		Double toleranceInMeters = (Double) intent.getExtras().get(Constants.INTENT_EXTRA_TOLERANCE);
-		Boolean vibrate = (Boolean) intent.getExtras().get(Constants.INTENT_EXTRA_VIBRATE);
-		Result result = null;
-		if (isRunning.compareAndSet(false, true)) {
-			this.startId = startId;
-			result = locationTracker.startTrackingByLocation(latitude, longitude, toleranceInMeters, vibrate);
-		} else if (!latitude.equals(locationTracker.getLatitude()) || !longitude.equals(locationTracker.getLongitude())
-			|| !toleranceInMeters.equals(locationTracker.getTolerance())
-			|| !vibrate.equals(locationTracker.shouldVibrate())) {
-			// already running, but the data has to be updated
-			result = locationTracker.startTrackingByLocation(latitude, longitude, toleranceInMeters, vibrate);
-		} else {
-			Logger.debug("LocationTrackerService is already running and nothing has to be updated - no action");
-		}
+        Double latitude = (Double) intent.getExtras().get(Constants.INTENT_EXTRA_LATITUDE);
+        Double longitude = (Double) intent.getExtras().get(Constants.INTENT_EXTRA_LONGITUDE);
+        Double toleranceInMeters = (Double) intent.getExtras().get(Constants.INTENT_EXTRA_TOLERANCE);
+        Boolean vibrate = (Boolean) intent.getExtras().get(Constants.INTENT_EXTRA_VIBRATE);
+        Result result = null;
+        if (isRunning.compareAndSet(false, true)) {
+            this.startId = startId;
+            result = locationTracker.startTrackingByLocation(latitude, longitude, toleranceInMeters, vibrate);
+        } else if (!latitude.equals(locationTracker.getLatitude()) || !longitude.equals(locationTracker.getLongitude())
+            || !toleranceInMeters.equals(locationTracker.getTolerance())
+            || !vibrate.equals(locationTracker.shouldVibrate())) {
+            // already running, but the data has to be updated
+            result = locationTracker.startTrackingByLocation(latitude, longitude, toleranceInMeters, vibrate);
+        } else {
+            Logger.debug("LocationTrackerService is already running and nothing has to be updated - no action");
+        }
 
-		if (result == Result.FAILURE_INSUFFICIENT_RIGHTS) {
-			// disable the tracking and notify user of it
-			basics.disableLocationBasedTracking();
-			basics
-				.showNotification(
-					"Disabling the location-based tracking because of missing privileges!",
-					"Disabled location-based tracking!",
-					"(open to see details)",
-					basics
-						.createMessagePendingIntent(
-							"Track Work Time disabled the location-based tracking because of missing privileges. You can re-enable it in the options and then grant the required permissions.",
-							Constants.MISSING_PRIVILEGE_ACCESS_COARSE_LOCATION_ID),
-					Constants.MISSING_PRIVILEGE_ACCESS_COARSE_LOCATION_ID, false, null, null, null, null, null, null);
-		}
+        if (result == Result.FAILURE_INSUFFICIENT_RIGHTS) {
+            // disable the tracking and notify user of it
+            basics.disableLocationBasedTracking();
+            basics
+                .showNotification(
+                    "Disabling the location-based tracking because of missing privileges!",
+                    "Disabled location-based tracking!",
+                    "(open to see details)",
+                    basics
+                        .createMessagePendingIntent(
+                            "Track Work Time disabled the location-based tracking because of missing privileges. You can re-enable it in the options and then grant the required permissions.",
+                            Constants.MISSING_PRIVILEGE_ACCESS_COARSE_LOCATION_ID),
+                    Constants.MISSING_PRIVILEGE_ACCESS_COARSE_LOCATION_ID, false, null, null, null, null, null, null);
+        } else if (result == Result.SUCCESS) {
+            Boolean notificationActive = basics.isNotificationActive(Constants.MISSING_PRIVILEGE_ACCESS_COARSE_LOCATION_ID);
+            if (notificationActive == null || notificationActive) {
+                basics.removeNotification(Constants.MISSING_PRIVILEGE_ACCESS_COARSE_LOCATION_ID);
+            }
+        }
 
-		return Service.START_NOT_STICKY;
-	}
+        return Service.START_NOT_STICKY;
+    }
 
-	@Override
-	public void onDestroy() {
-		Logger.info("destroying LocationTrackerService");
-		locationTracker.stopTrackingByLocation();
-		isRunning.set(false);
-		stopSelf();
-	}
+    @Override
+    public void onDestroy() {
+        Logger.info("destroying LocationTrackerService");
+        locationTracker.stopTrackingByLocation();
+        isRunning.set(false);
+        stopSelf();
+    }
 
 }
--- app/src/main/java/org/zephyrsoft/trackworktime/location/WifiTrackerService.java
+++ app/src/main/java/org/zephyrsoft/trackworktime/location/WifiTrackerService.java
@@ -1,16 +1,16 @@
 /*
  * This file is part of TrackWorkTime (TWT).
- * 
+ *
  * TWT is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * TWT is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with TWT. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -32,110 +32,115 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * The background service providing the wifi-based tracking without having the activity open.
- * 
+ *
  * @author Christoph Loewe
  */
 public class WifiTrackerService extends Service {
 
-	private static WifiTracker wifiTracker = null;
-	private int startId;
+    private static WifiTracker wifiTracker = null;
+    private int startId;
 
-	private static final AtomicBoolean isRunning = new AtomicBoolean(false);
+    private static final AtomicBoolean isRunning = new AtomicBoolean(false);
 
-	private Basics basics = null;
-	private WifiScanner wifiScanner;
+    private Basics basics = null;
+    private WifiScanner wifiScanner;
 
-	@Override
-	public void onCreate() {
-		Logger.info("creating WifiTrackerService");
-		basics = Basics.getOrCreateInstance(getApplicationContext());
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-			startForeground(Constants.PERSISTENT_TRACKING_ID, basics.createNotificationTracking());
-		}
+    @Override
+    public void onCreate() {
+        Logger.info("creating WifiTrackerService");
+        basics = Basics.getOrCreateInstance(getApplicationContext());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForeground(Constants.PERSISTENT_TRACKING_ID, basics.createNotificationTracking());
+        }
 
-		WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-		int time = (int)(Constants.REPEAT_TIME / 1000 / 2);
-		wifiScanner = new WifiScanner(wifiManager, time, time);
-		wifiScanner.register(getApplicationContext());
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        int time = (int) (Constants.REPEAT_TIME / 1000 / 2);
+        wifiScanner = new WifiScanner(wifiManager, time, time);
+        wifiScanner.register(getApplicationContext());
 
-		wifiTracker = new WifiTracker(
-				basics.getTimerManager(),
-				basics.getExternalNotificationManager(),
-				(AudioManager) getSystemService(Context.AUDIO_SERVICE),
-				wifiScanner);
+        wifiTracker = new WifiTracker(
+            basics.getTimerManager(),
+            basics.getExternalNotificationManager(),
+            (AudioManager) getSystemService(Context.AUDIO_SERVICE),
+            wifiScanner);
 
-		// restart if service crashed previously
-		Basics.getOrCreateInstance(getApplicationContext()).safeCheckWifiBasedTracking();
-	}
+        // restart if service crashed previously
+        Basics.getOrCreateInstance(getApplicationContext()).safeCheckWifiBasedTracking();
+    }
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		// do nothing here as we don't bind the service to an activity
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        // do nothing here as we don't bind the service to an activity
+        return null;
+    }
 
-	// START_STICKY : Using this return value, if the OS kills our Service it will recreate it but the Intent that was
-	// sent to the Service isn’t redelivered. In this way the Service is always running
-	// START_NOT_STICKY: If the SO kills the Service it won’t recreate it until the client calls explicitly onStart
-	// command
-	// START_REDELIVER_INTENT: It is similar to the START_STICKY and in this case the Intent will be redelivered to the
-	// service.
-	@Override
-	public int onStartCommand(Intent intent, int flags, @SuppressWarnings("hiding") int startId) {
-		if (intent == null || intent.getExtras() == null) {
-			// something went wrong, quit here
-			return Service.START_NOT_STICKY;
-		}
+    // START_STICKY : Using this return value, if the OS kills our Service it will recreate it but the Intent that was
+    // sent to the Service isn’t redelivered. In this way the Service is always running
+    // START_NOT_STICKY: If the SO kills the Service it won’t recreate it until the client calls explicitly onStart
+    // command
+    // START_REDELIVER_INTENT: It is similar to the START_STICKY and in this case the Intent will be redelivered to the
+    // service.
+    @Override
+    public int onStartCommand(Intent intent, int flags, @SuppressWarnings("hiding") int startId) {
+        if (intent == null || intent.getExtras() == null) {
+            // something went wrong, quit here
+            return Service.START_NOT_STICKY;
+        }
 
-		String ssid = (String) intent.getExtras().get(Constants.INTENT_EXTRA_SSID);
-		Boolean vibrate = (Boolean) intent.getExtras().get(Constants.INTENT_EXTRA_VIBRATE);
-		Result result = null;
-		if (isRunning.compareAndSet(false, true)) {
-			this.startId = startId;
-			result = wifiTracker.startTrackingByWifi(ssid, vibrate);
-		} else if (!ssid.equals(wifiTracker.getSSID()) || !vibrate.equals(wifiTracker.shouldVibrate())) {
-			// already running, but the data has to be updated
-			result = wifiTracker.startTrackingByWifi(ssid, vibrate);
-		} else {
-			Logger.debug("WifiTrackerService is already running and nothing has to be updated - no action");
-		}
+        String ssid = (String) intent.getExtras().get(Constants.INTENT_EXTRA_SSID);
+        Boolean vibrate = (Boolean) intent.getExtras().get(Constants.INTENT_EXTRA_VIBRATE);
+        Result result = null;
+        if (isRunning.compareAndSet(false, true)) {
+            this.startId = startId;
+            result = wifiTracker.startTrackingByWifi(ssid, vibrate);
+        } else if (!ssid.equals(wifiTracker.getSSID()) || !vibrate.equals(wifiTracker.shouldVibrate())) {
+            // already running, but the data has to be updated
+            result = wifiTracker.startTrackingByWifi(ssid, vibrate);
+        } else {
+            Logger.debug("WifiTrackerService is already running and nothing has to be updated - no action");
+        }
 
-		if (result == Result.FAILURE_INSUFFICIENT_RIGHTS) {
-			// disable the tracking and notify user of it
-			basics.disableWifiBasedTracking();
-			basics.showNotification(
-					"Disabling the wifi-based tracking because of missing privileges!",
-					"Disabled wifi-based tracking!",
-					"(open to see details)",
-					basics
-						.createMessagePendingIntent(
-							"Track Work Time disabled the wifi-based tracking because of missing privileges. You can re-enable it in the options and then grant the required permissions.",
-							Constants.MISSING_PRIVILEGE_ACCESS_WIFI_STATE_ID),
-					Constants.MISSING_PRIVILEGE_ACCESS_WIFI_STATE_ID, false, null, null, null, null, null, null);
-		}
+        if (result == Result.FAILURE_INSUFFICIENT_RIGHTS) {
+            // disable the tracking and notify user of it
+            basics.disableWifiBasedTracking();
+            basics.showNotification(
+                "Disabling the wifi-based tracking because of missing privileges!",
+                "Disabled wifi-based tracking!",
+                "(open to see details)",
+                basics
+                    .createMessagePendingIntent(
+                        "Track Work Time disabled the wifi-based tracking because of missing privileges. You can re-enable it in the options and then grant the required permissions.",
+                        Constants.MISSING_PRIVILEGE_ACCESS_WIFI_STATE_ID),
+                Constants.MISSING_PRIVILEGE_ACCESS_WIFI_STATE_ID, false, null, null, null, null, null, null);
+        } else if (result == Result.SUCCESS) {
+            Boolean notificationActive = basics.isNotificationActive(Constants.MISSING_PRIVILEGE_ACCESS_WIFI_STATE_ID);
+            if (notificationActive == null || notificationActive) {
+                basics.removeNotification(Constants.MISSING_PRIVILEGE_ACCESS_WIFI_STATE_ID);
+            }
+        }
 
-		// the check for available wifi networks is done here (and thus needs the periodically sent intents):
-		checkWifiIfEnabled();
+        // the check for available wifi networks is done here (and thus needs the periodically sent intents):
+        checkWifiIfEnabled();
 
-		return Service.START_NOT_STICKY;
-	}
+        return Service.START_NOT_STICKY;
+    }
 
-	private void checkWifiIfEnabled() {
-		if (isRunning.get()) {
-			wifiTracker.checkWifi();
-		}
-	}
+    private void checkWifiIfEnabled() {
+        if (isRunning.get()) {
+            wifiTracker.checkWifi();
+        }
+    }
 
-	@Override
-	public void onDestroy() {
-		Logger.info("destroying WifiTrackerService");
-		wifiTracker.stopTrackingByWifi();
+    @Override
+    public void onDestroy() {
+        Logger.info("destroying WifiTrackerService");
+        wifiTracker.stopTrackingByWifi();
 
-		wifiScanner.unregister(getApplicationContext());
-		wifiScanner.setWifiScanListener(null);
+        wifiScanner.unregister(getApplicationContext());
+        wifiScanner.setWifiScanListener(null);
 
-		isRunning.set(false);
-		stopSelf();
-	}
+        isRunning.set(false);
+        stopSelf();
+    }
 
 }
