260732972a6b08b62055f1ad6d6cd74b6f4b28ee
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2021-04-05 15:45:49 +0200

    fix version to GPL 3.0 (drop "either version 3 of the License, or (at your option) any later version" clause)

--- app/src/main/java/org/zephyrsoft/trackworktime/backup/BackupFileInfo.java
+++ app/src/main/java/org/zephyrsoft/trackworktime/backup/BackupFileInfo.java
@@ -1,3 +1,18 @@
+/*
+ * This file is part of TrackWorkTime (TWT).
+ *
+ * TWT is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License 3.0 as published by
+ * the Free Software Foundation.
+ *
+ * TWT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License 3.0 for more details.
+ *
+ * You should have received a copy of the GNU General Public License 3.0
+ * along with TWT. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.zephyrsoft.trackworktime.backup;
 
 import android.os.Environment;
--- app/src/main/java/org/zephyrsoft/trackworktime/report/CsvGenerator.java
+++ app/src/main/java/org/zephyrsoft/trackworktime/report/CsvGenerator.java
@@ -2,16 +2,15 @@
  * This file is part of TrackWorkTime (TWT).
  * 
  * TWT is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * under the terms of the GNU General Public License 3.0 as published by
+ * the Free Software Foundation.
  * 
  * TWT is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU General Public License 3.0 for more details.
  * 
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU General Public License 3.0
  * along with TWT. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.zephyrsoft.trackworktime.report;
